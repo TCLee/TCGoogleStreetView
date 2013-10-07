446935acfd414cbc753514f0148414631fb7eca2
Lee Tze Cheun <leetzecheun@gmail.com>
2013-10-07 16:49:35 +0800

    Print description for array of GMSPanoramaLink objects.

--- TCGoogleStreetView/Lib/GMSPanorama+Debug.m
+++ TCGoogleStreetView/Lib/GMSPanorama+Debug.m
@@ -17,8 +17,26 @@
     [description appendFormat:@"GMSPanorama <%p>\n", (void *)self];
     [description appendString:@"{\n"];
     [description appendFormat:@"  panoramaID: %@,\n", self.panoramaID];
-    [description appendFormat:@"  coordinate: (%@, %@)\n", [@(self.coordinate.latitude) stringValue], [@(self.coordinate.longitude) stringValue]];
+    [description appendFormat:@"  coordinate: (%@, %@),\n", [@(self.coordinate.latitude) stringValue], [@(self.coordinate.longitude) stringValue]];
+    [description appendFormat:@"%@", [self descriptionForLinks]];
     [description appendString:@"}\n"];
+    
+    return [description copy];
+}
+
+- (NSString *)descriptionForLinks
+{
+    NSMutableString *description = [[NSMutableString alloc] init];
+    
+    [description appendString:@"  links: {\n"];
+    for (GMSPanoramaLink *link in self.links) {
+        [description appendFormat:@"    GMSPanoramaLink <%p>\n", (void *)self];
+        [description appendString:@"    {\n"];
+        [description appendFormat:@"      heading: %@\n", [@(link.heading) stringValue]];
+        [description appendFormat:@"      panoramaID: %@,\n", link.panoramaID];
+        [description appendString:@"    },\n"];
+    }
+    [description appendString:@"  }\n"];
     
     return [description copy];
 }
