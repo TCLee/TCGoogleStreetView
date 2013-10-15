cfa5d17ff09015f334f27bed9e5971f26b337f73
Lee Tze Cheun <leetzecheun@gmail.com>
2013-10-15 10:53:13 +0800

    Returned a better formatted description of a GMSPanoramaLayer instance.

--- TCGoogleStreetView/Lib/GMSPanoramaLayer+Debug.m
+++ TCGoogleStreetView/Lib/GMSPanoramaLayer+Debug.m
@@ -16,25 +16,24 @@
     
     [description appendFormat:@"GMSPanoramaLayer <%p>\n", (void *)self];
     [description appendString:@"{\n"];
-    [description appendFormat:@"  kGMSLayerPanoramaHeadingKey: \"%@\",\n", kGMSLayerPanoramaHeadingKey];
-    [description appendFormat:@"  kGMSLayerPanoramaPitchKey: \"%@\",\n", kGMSLayerPanoramaPitchKey];
-    [description appendFormat:@"  kGMSLayerPanoramaZoomKey: \"%@\",\n", kGMSLayerPanoramaZoomKey];
-    [description appendFormat:@"  kGMSLayerPanoramaFOVKey: \"%@\",\n\n", kGMSLayerPanoramaFOVKey];
-    
-    [description appendFormat:@"  cameraHeading: %@,\n", [@(self.cameraHeading) stringValue]];
-    [description appendFormat:@"  cameraPitch: %@,\n", [@(self.cameraPitch) stringValue]];
-    [description appendFormat:@"  cameraZoom: %@,\n", [@(self.cameraZoom) stringValue]];
-    [description appendFormat:@"  cameraFOV: %@,\n\n", [@(self.cameraFOV) stringValue]];
-
-    [description appendFormat:@"  position: %@,\n", NSStringFromCGPoint(self.position)];
-    [description appendFormat:@"  bounds: %@,\n", NSStringFromCGRect(self.bounds)];
-    [description appendFormat:@"  anchorPoint: %@,\n", NSStringFromCGPoint(self.anchorPoint)];
-    [description appendFormat:@"  opaque: %@,\n", self.opaque ? @"YES" : @"NO"];
     [description appendFormat:@"  animationKeys: %@,\n", self.animationKeys];
-    [description appendFormat:@"  actions: %@,\n", self.actions];
-    [description appendFormat:@"  style: %@,\n", self.style];
+    [description appendFormat:@"  presentationLayer: %@,\n", [self descriptionForLayer:self.presentationLayer]];
+    [description appendFormat:@"  modelLayer: %@,\n", [self descriptionForLayer:self.modelLayer]];
     [description appendString:@"}\n"];
     
+    return [description copy];
+}
+
+- (NSString *)descriptionForLayer:(GMSPanoramaLayer *)layer
+{
+    NSMutableString *description = [[NSMutableString alloc] init];
+    [description appendFormat:@"%@ <%p>\n", NSStringFromClass([layer class]), (void *)layer];
+    [description appendString:@"  {\n"];
+    [description appendFormat:@"    cameraHeading: %@,\n", [@(layer.cameraHeading) stringValue]];
+    [description appendFormat:@"    cameraPitch: %@,\n", [@(layer.cameraPitch) stringValue]];
+    [description appendFormat:@"    cameraZoom: %@,\n", [@(layer.cameraZoom) stringValue]];
+    [description appendFormat:@"    cameraFOV: %@,\n", [@(layer.cameraFOV) stringValue]];
+    [description appendString:@"  }"];
     return [description copy];
 }
 
